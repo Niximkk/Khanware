5eaf7bea7539912c31940af2ef0031fcedbaedc5
LuizzLoL <143542907+LuizzLoL@users.noreply.github.com>
2025-06-08 14:31:42 -0300

    Update mainMenu.js

--- visuals/mainMenu.js
+++ visuals/mainMenu.js
@@ -6,7 +6,7 @@ let featuresList = [
     { name: 'repeatQuestion', className: 'repeatQuestion', type: 'checkbox', variable: 'features.repeatQuestion', attributes: 'style="display:none;"', labeled: true, label: 'Repeat Question' },
     { name: 'nextRecomendation', className: 'nextRecomendation', type: 'checkbox', variable: 'features.nextRecomendation', attributes: 'style="display:none;"', labeled: true, label: 'Recomendations' },
     { name: 'autoAnswerDelay', className: 'autoAnswerDelay', type: 'range', variable: 'features.autoAnswerDelay', attributes: 'style="display:none;" min="1" max="3" value="1"', labeled: false },
-    { name: 'Accuracy', className: 'Accuracy', type: 'range', variable: 'features.Accuracy', min: 0, max: 100, value: 100, labeled: true, label: 'Accuracy (0-100%)' }, // Alterei para um 'range' com valor padrão de 100
+    { name: 'Accuracy', className: 'Accuracy', type: 'checkbox', variable: 'features.Accuracy', attributes: 'style="display:none;"', labeled: true, label: 'Accuracy (0% - 100%)' }, 
     { name: 'minuteFarm', type: 'checkbox', variable: 'features.minuteFarmer', labeled: true, label: 'Minute Farmer' },
     { name: 'customBanner', type: 'checkbox', variable: 'features.customBanner', labeled: true, label: 'Custom Banner' },
     { name: 'rgbLogo', type: 'checkbox', variable: 'features.rgbLogo', labeled: true, label: 'RGB Logo' },
