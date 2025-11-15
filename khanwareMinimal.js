317ad9bd7f06b87ca6d8557e539d09e57d3ee89c
Nix <85513545+Niximkk@users.noreply.github.com>
2025-11-15 00:56:39 -0300

    Hotfix
    Forgot that the features are not an option in here.

--- khanwareMinimal.js
+++ khanwareMinimal.js
@@ -48,7 +48,7 @@ function setupMain(){
             const url = input instanceof Request ? input.url : input;
             let body = input instanceof Request ? await input.clone().text() : init?.body;
             
-            if (features.questionSpoof && url.includes('getAssessmentItem') && body) {
+            if (url.includes('getAssessmentItem') && body) {
                 const res = await originalFetch.apply(this, arguments);
                 const clone = res.clone();
                 
@@ -120,7 +120,7 @@ function setupMain(){
                 return res;
             }
             
-            if (features.questionSpoof && body?.includes('"operationName":"attemptProblem"')) {
+            if (body?.includes('"operationName":"attemptProblem"')) {
                 try {
                     let bodyObj = JSON.parse(body);
                     const itemId = bodyObj.variables?.input?.assessmentItemId;
@@ -224,20 +224,14 @@ function setupMain(){
         khanwareDominates = true;
 
         (async () => { 
-            while (khanwareDominates) {
-                if (features.autoAnswer && features.questionSpoof) {
-                    
-                    const selectorsToCheck = [...baseSelectors];
-
-                    if (features.nextRecomendation) selectorsToCheck.push("._g9riz5o")
-                    if (features.repeatQuestion) selectorsToCheck.push("._10goqnzn");
-
-                    for (const q of selectorsToCheck) {
-                        findAndClickBySelector(q);
-                        if (document.querySelector(q+"> div") && document.querySelector(q+"> div").innerText === "Mostrar resumo") {
-                            sendToast("🎉 Exercício concluído!", 3000);
-                            playAudio("https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/4x5g14gj.wav");
-                        }
+            while (khanwareDominates) {                    
+                const selectorsToCheck = [...baseSelectors];
+                
+                for (const q of selectorsToCheck) {
+                    findAndClickBySelector(q);
+                    if (document.querySelector(q+"> div") && document.querySelector(q+"> div").innerText === "Mostrar resumo") {
+                        sendToast("🎉 Exercício concluído!", 3000);
+                        playAudio("https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/4x5g14gj.wav");
                     }
                 }
                 await delay(featureConfigs.autoAnswerDelay * 800);
