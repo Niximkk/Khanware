da16666e99bcf6a68e95fd9b1f7c0a5442dca17a
Nix <85513545+Niximkk@users.noreply.github.com>
2026-01-07 03:41:19 -0300

    Change homepage role to LEARNER in enableRoles
    Updated the enableRoles function to set the homepage to 'LEARNER' instead of 'COACH' when updating user roles and homepage settings.

--- visuals/tweaksTab.js
+++ visuals/tweaksTab.js
@@ -17,7 +17,7 @@ plppdo.on('domChanged', () => {
 });
 
 function enableRoles() {
-    fetch('https://pt.khanacademy.org/api/internal/graphql/UpdateRolesAndHomepage', { method: "POST", credentials: "include", headers: { "x-ka-fkey": "1" }, body: '{"operationName":"UpdateRolesAndHomepage","query":"mutation UpdateRolesAndHomepage($roles: [UserRole]!, $homepage: UserHomepage!) {\\n  updateRolesAndHomepageSettings(roles: $roles, homepage: $homepage) {\\n    user {\\n      id\\n      isTeacher\\n      isParent\\n      homepageUrl\\n      __typename\\n    }\\n    error {\\n      code\\n      __typename\\n    }\\n    __typename\\n  }\\n}","variables":{"roles":["TEACHER","PARENT"],"homepage":"COACH"}}' })
+    fetch('https://pt.khanacademy.org/api/internal/graphql/UpdateRolesAndHomepage', { method: "POST", credentials: "include", headers: { "x-ka-fkey": "1" }, body: '{"operationName":"UpdateRolesAndHomepage","query":"mutation UpdateRolesAndHomepage($roles: [UserRole]!, $homepage: UserHomepage!) {\\n  updateRolesAndHomepageSettings(roles: $roles, homepage: $homepage) {\\n    user {\\n      id\\n      isTeacher\\n      isParent\\n      homepageUrl\\n      __typename\\n    }\\n    error {\\n      code\\n      __typename\\n    }\\n    __typename\\n  }\\n}","variables":{"roles":["TEACHER","PARENT"],"homepage":"LEARNER"}}' })
     .then(async () => { sendToast(`🪄 ${t('roles_enabled')}`, 3000); })
     .catch(e => { debug(`🚨 ${t('error_at')} tweaksTab.js\n${e}`); });
 }
